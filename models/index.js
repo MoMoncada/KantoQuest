6e05da03ba6ac594ad6584dbddf27fe03b4b209c
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-02-28 20:11:57 +0800

    init User model

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const Pokedex = require('./Pokedex')
+const User = require('./Users')
+const Party = require('./Party')
+const Pokemon = require('./Pokemon')
+
+User.hasOne(Pokedex, {
+    foreignKey: 'user_id'
+    onDelete: 'CASCADE'
+})
