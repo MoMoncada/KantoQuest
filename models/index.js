edd5b86dda0d695c53c0c3bc9808d817b9dd04cb
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-02-28 21:11:33 +0800

    init model index

--- models/index.js
+++ models/index.js
@@ -1,9 +1,51 @@
 const Pokedex = require('./Pokedex')
-const User = require('./Users')
+const User = require('./User')
 const Party = require('./Party')
 const Pokemon = require('./Pokemon')
 
+// User has one Pokedex
 User.hasOne(Pokedex, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 })
+
+Pokedex.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+// User has many parties
+User.hasMany(Party, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+Party.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+// Pokedex can have many Pokemon
+Pokedex.hasMany(Pokemon, {
+    foreignKey: 'pokemon_id',
+    onDelete: 'SET NULL'
+})
+
+// Pokemon can belong to many Pokedex
+Pokemon.belongsToMany(Pokedex, {
+    through: Pokedex_pokemon
+})
+
+Pokedex.belongsToMany(Pokemon, {
+    through: Pokedex_pokemon
+})
+
+Pokemon.belongsToMany(Party, {
+    through: Party_pokemon
+})
+
+Party.belongsToMany(Pokemon, {
+    through: Party_pokemon
+})
+
+
+
+Pokemon.belongsToMany()
