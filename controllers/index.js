d3c8ec6916d2649da24623853d55695ee5a20a5b
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-03-09 19:02:11 +0800

    battle routes, battle page rendering now

--- controllers/index.js
+++ controllers/index.js
@@ -6,10 +6,12 @@ const apiRoutes = require("./api/");
 const dashboardRoutes = require("./dashboardRoutes");
 const homeRoutes = require("./homeRoutes");
 const pokedexRoutes = require('./pokedexRoutes');
+const battleRoute = require('./battleRoute');
 
 router.use("/api", apiRoutes);
 router.use("/dashboard", dashboardRoutes);
 router.use('/pokedex', pokedexRoutes);
 router.use("/", homeRoutes);
+router.use("/battle", battleRoute);
 
 module.exports = router;
