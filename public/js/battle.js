64f3dba8075a922a7ad2f5e847f108322c494993
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-03-11 15:43:36 +0800

    Battle page is now rendering the pokemon we select from our party and starts by default with a placeholder img

--- public/js/battle.js
+++ public/js/battle.js
@@ -1,1 +1,10 @@
-// Empty
+function selectPokemon() {
+    const yourPokemonImg = document.getElementById("your-pokemon");
+    yourPokemonImg.src = this.src;
+  }
+  
+  const myPartyPokemonImgs = document.querySelectorAll("#my-party");
+  
+  myPartyPokemonImgs.forEach(pokemonImg => {
+    pokemonImg.addEventListener("click", selectPokemon);
+  });
