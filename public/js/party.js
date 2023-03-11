6b8d8bb93785cab79e3feafa9d0cff080f6d6b0c
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-03-11 12:32:23 +0800

    css changes, add/remove buttons are working correctly and staying where they are supposed to live

--- public/js/party.js
+++ public/js/party.js
@@ -30,35 +30,88 @@
 
 
 // Func for Removing from Party
-const delPokemonBtnHandler = async (event) => {
-    if (event.target.hasAttribute("data-id")) {
-      const id = event.target.getAttribute("data-id");
-      const pokemon_id = event.target.getAttribute("data-pokemon-id");
-      const response = await fetch(`/api/trainerPokedex/remove/${id}`, {
-        method: "PUT",
-        body: JSON.stringify({ pokemon_id }),
-        headers: { "Content-Type": "application/json" },
-      });
+// const delPokemonBtnHandler = async (event) => {
+//     if (event.target.hasAttribute("data-id")) {
+//       const id = event.target.getAttribute("data-id");
+//       const pokemon_id = event.target.getAttribute("data-pokemon-id");
+//       const response = await fetch(`/api/trainerPokedex/remove/${id}`, {
+//         method: "PUT",
+//         body: JSON.stringify({ pokemon_id }),
+//         headers: { "Content-Type": "application/json" },
+//       });
 
-      if (response.ok) {
-        document.location.reload();
-      } else {
-        alert("Error removing from party: " + response);
-      }
-    }
-  };
+//       if (response.ok) {
+//         document.location.reload();
+//       } else {
+//         alert("Error removing from party: " + response);
+//       }
+//     }
+//   };
+
+// document.querySelectorAll(".remove-pokemon").forEach((delPokemon) => {
+//     delPokemon.addEventListener("click", delPokemonBtnHandler);
+//   });
+
+
+const delPokemonBtnHandler = async (event) => {
+   if (event.target.hasAttribute("data-id")) {
+     const id = event.target.getAttribute("data-id");
+     const pokemon_id = event.target.getAttribute("data-pokemon-id");
+     const button = event.target;
+     button.disabled = true; // disable button to prevent multiple clicks
+  
+     const response = await fetch(`/api/trainerPokedex/remove/${id}`, {
+       method: "PUT",
+       body: JSON.stringify({ pokemon_id }),
+       headers: { "Content-Type": "application/json" },
+     });
+ 
+     if (response.ok) {
+       document.location.reload();
+     } else {
+       alert("Error removing from party: " + response);
+       button.disabled = false; // re-enable button if there's an error
+     }
+   }
+};
 
 document.querySelectorAll(".remove-pokemon").forEach((delPokemon) => {
-    delPokemon.addEventListener("click", delPokemonBtnHandler);
-  });
+      delPokemon.addEventListener("click", delPokemonBtnHandler);
+});
+
+
 
 
 // Func for Adding to the Party
 
+// const addPokemonBtnHandler = async (event) => {
+//     if (event.target.hasAttribute("data-id")) {
+//       const id = event.target.getAttribute("data-id");
+//       const pokemon_id = event.target.getAttribute("data-pokemon-id");
+//       const response = await fetch(`/api/trainerPokedex/add/${id}`, {
+//         method: "PUT",
+//         body: JSON.stringify({ pokemon_id }),
+//         headers: { "Content-Type": "application/json" },
+//       });
+
+//       if (response.ok) {
+//         document.location.reload();
+//       } else {
+//         alert("Error adding to party: " + response);
+//       }
+//     }
+//   };
+
+
+  
+
+
 const addPokemonBtnHandler = async (event) => {
-    if (event.target.hasAttribute("data-id")) {
-      const id = event.target.getAttribute("data-id");
-      const pokemon_id = event.target.getAttribute("data-pokemon-id");
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+    const pokemon_id = event.target.getAttribute("data-pokemon-id");
+    const partyBox = document.getElementById("party-box");
+    if (partyBox.children.length < 6) {
       const response = await fetch(`/api/trainerPokedex/add/${id}`, {
         method: "PUT",
         body: JSON.stringify({ pokemon_id }),
@@ -70,10 +123,13 @@ const addPokemonBtnHandler = async (event) => {
       } else {
         alert("Error adding to party: " + response);
       }
+    } else {
+      alert("You already have 6 pokemon in your party!");
     }
-  };
+  }
+};
+
 
 document.querySelectorAll(".add-pokemon").forEach((addPokemon) => {
-    addPokemon.addEventListener("click", addPokemonBtnHandler);
-  });
-  
+  addPokemon.addEventListener("click", addPokemonBtnHandler);
+});
